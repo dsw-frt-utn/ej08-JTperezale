aac33e787669cc2128c8ecf370233335e7dc801b
Joaquin Tomas Perez Ale <joaquinperezale@gmail.com>
2026-05-02 03:18:04 -0300

    Resolucion Problema 2

--- Dsw2026Ej8/Program.cs
+++ Dsw2026Ej8/Program.cs
@@ -4,11 +4,19 @@
     {
         static void Main(string[] args)
         {
+            // Problema 1
             var helper = new ProductHelper();
-
             Console.WriteLine(helper.ObtenerEtiquetaProducto(1001, "Coca Cola 500ml", 1250.99m));
             Console.WriteLine(helper.ObtenerEtiquetaProducto(1, "Agua mineral", 500m));
             Console.WriteLine(helper.ObtenerEtiquetaProducto(7, "Auriculares", 12999.99m));
+
+            Console.WriteLine("---");
+
+            // Problema 2
+            var p2 = new Problema2();
+            Console.WriteLine(p2.CrearResumenVenta(1001, "Coca Cola", 5, 1250.99m));
+            Console.WriteLine(p2.CrearResumenVenta(2002, "Agua mineral", -1, 500m));
+            Console.WriteLine(p2.CrearResumenVenta(3003, "Auriculares", 2, 12999.99m));
         }
     }
 }
